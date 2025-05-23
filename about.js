e92d9ae12865c57a458aea9b6cbb1787e19e8239
faisalakhmad888 <faisal.akhmad404@gmail.com>
2025-05-23 11:40:44 +0700

    Add files via upload

--- about.js
+++ about.js
@@ -5,8 +5,8 @@ export default function About() {
   return (
     <Layout>
       <h2>About Me</h2>
-      <p>Hi! I&#39;m Faisal Akhmad, a Senior UI/UX Designer with over 6 years of experience.</p>
-      <p>II&#39ve worked across the Telco, F&B, and Finance industries to design powerful digital products.</p>
+      <p>Hi! Im Faisal Akhmad, a Senior UI/UX Designer with over 6 years of experience.</p>
+      <p>Ive worked across the Telco, F&B, and Finance industries to design powerful digital products.</p>
       // <p>
       //   <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
       //     ← Back to homepage
